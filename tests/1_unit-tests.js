b553d5cb0c1885014de65c5f123978090c3902de
Fernando <palhares.f@gmail.com>
2025-01-02 15:57:31 -0300

    All unit tests finalized

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -63,53 +63,58 @@ suite('Unit Tests', () => {
       assert.strictEqual(result, 'Lunch is at <span class="highlight">12.15</span> today.');
     });
 
-    // test('Translate We watched the footie match for a while. to American English', () => {
-    //   const result = translator.translate('We watched the footie match for a while.', 'british-to-american');
-    //   assert.strictEqual(result, 'We watched the <span class="highlight">soccer</span> match for a while.');
-    // });
+    test('Translate We watched the footie match for a while. to American English', () => {
+      const result = translator.translate('We watched the footie match for a while.', 'british-to-american');
+      assert.strictEqual(result, 'We watched the <span class="highlight">soccer</span> match for a while.');
+    });
 
-    // test('Translate Paracetamol takes up to an hour to work. to American English', () => {
-    //   const result = translator.translate('Paracetamol takes up to an hour to work.', 'british-to-american');
-    //   assert.strictEqual(result, '<span class="highlight">Tylenol</span> takes up to an hour to work.');
-    // });
+    test('Translate Paracetamol takes up to an hour to work. to American English', () => {
+      const result = translator.translate('Paracetamol takes up to an hour to work.', 'british-to-american');
+      assert.strictEqual(result, '<span class="highlight">Tylenol</span> takes up to an hour to work.');
+    });
 
-    // test('Translate First, caramelise the onions. to American English', () => {
-    //   const result = translator.translate('First, caramelise the onions.', 'british-to-american');
-    //   assert.strictEqual(result, 'First, <span class="highlight">caramelize</span> the onions.');
-    // });
+    test('Translate First, caramelise the onions. to American English', () => {
+      const result = translator.translate('First, caramelise the onions.', 'british-to-american');
+      assert.strictEqual(result, 'First, <span class="highlight">caramelize</span> the onions.');
+    });
 
-    // test('Translate I spent the bank holiday at the funfair. to American English', () => {
-    //   const result = translator.translate('I spent the bank holiday at the funfair.', 'british-to-american');
-    //   assert.strictEqual(result, 'I spent the <span class="highlight">holiday</span> at the <span class="highlight">carnival</span>.');
-    // });
+    test('Translate I spent the bank holiday at the funfair. to American English', () => {
+      const result = translator.translate('I spent the bank holiday at the funfair.', 'british-to-american');
+      assert.strictEqual(result, 'I spent the <span class="highlight">public holiday</span> at the <span class="highlight">carnival</span>.');
+    });
 
-    // test('Translate I\'ve just got bits and bobs in my bum bag. to American English', () => {
-    //   const result = translator.translate('I\'ve just got bits and bobs in my bum bag.', 'british-to-american');
-    //   assert.strictEqual(result, 'I\'ve just got bits and bobs in my <span class="highlight">fanny pack</span>.');
-    // });
+    test('Translate I had a bicky then went to the chippy. to American English', () => {
+      const result = translator.translate('I had a bicky then went to the chippy.', 'british-to-american');
+      assert.strictEqual(result, 'I had a <span class="highlight">cookie</span> then went to the <span class="highlight">fish-and-chip shop</span>.');
+    });
 
-    // test('Translate The car boot sale at Boxted Airfield was called off. to American English', () => {
-    //   const result = translator.translate('The car boot sale at Boxted Airfield was called off.', 'british-to-american');
-    //   assert.strictEqual(result, 'The <span class="highlight">swap meet</span> at Boxted Airfield was called off.');
-    // });
+    test('Translate I\'ve just got bits and bobs in my bum bag. to American English', () => {
+      const result = translator.translate('I\'ve just got bits and bobs in my bum bag.', 'british-to-american');
+      assert.strictEqual(result, 'I\'ve just got <span class="highlight">odds and ends</span> in my <span class="highlight">fanny pack</span>.');
+    });
 
-    // test('Translate Have you met Mrs Kalyani? to American English', () => {
-    //   const result = translator.translate('Have you met Mrs Kalyani?', 'british-to-american');
-    //   assert.strictEqual(result, 'Have you met <span class="highlight">Mrs.</span> Kalyani?');
-    // });
+    test('Translate The car boot sale at Boxted Airfield was called off. to American English', () => {
+      const result = translator.translate('The car boot sale at Boxted Airfield was called off.', 'british-to-american');
+      assert.strictEqual(result, 'The <span class="highlight">swap meet</span> at Boxted Airfield was called off.');
+    });
 
-    // test('Translate Prof Joyner of King\'s College, London. to American English', () => {
-    //   const result = translator.translate('Prof Joyner of King\'s College, London.', 'british-to-american');
-    //   assert.strictEqual(
-    //     result,
-    //     '<span class="highlight">Prof.</span> Joyner of King\'s College, London.'
-    //   );
-    // });
+    test('Translate Have you met Mrs Kalyani? to American English', () => {
+      const result = translator.translate('Have you met Mrs Kalyani?', 'british-to-american');
+      assert.strictEqual(result, 'Have you met <span class="highlight">Mrs.</span> Kalyani?');
+    });
 
-    // test('Translate Tea time is usually around 4 or 4.30. to American English', () => {
-    //   const result = translator.translate('Tea time is usually around 4 or 4.30.', 'british-to-american');
-    //   assert.strictEqual(result, 'Tea time is usually around 4 or <span class="highlight">4:30.</span>');
-    // });
+    test('Translate Prof Joyner of King\'s College, London. to American English', () => {
+      const result = translator.translate('Prof Joyner of King\'s College, London.', 'british-to-american');
+      assert.strictEqual(
+        result,
+        '<span class="highlight">Prof.</span> Joyner of King\'s College, London.'
+      );
+    });
+
+    test('Translate Tea time is usually around 4 or 4.30. to American English', () => {
+      const result = translator.translate('Tea time is usually around 4 or 4.30.', 'british-to-american');
+      assert.strictEqual(result, 'Tea time is usually around 4 or <span class="highlight">4:30</span>.');
+    });
 
 });
 
