59bbb9701c019b22af1cd8d4ccc6147d41031a71
Fernando <palhares.f@gmail.com>
2025-01-02 12:17:54 -0300

    Basic implementation of translation, still missing highlights and some type fixing on the API routes

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,38 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  static locales = ["american-to-british", "british-to-american"];
 
+  translate(text, locale) {
+    if (locale === "american-to-british") {
+      const words = text.toLowerCase().split(" ");
+      for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        if (americanToBritishTitles[word]) {
+          words[i] = americanToBritishTitles[word];
+        } else if (americanToBritishSpelling[word]) {
+          words[i] = americanToBritishSpelling[word];
+        } else if (americanOnly[word]) {
+          words[i] = americanOnly[word];
+        }
+      }
+      return words.join(" ");
+    } else if (locale === "british-to-american") {
+      const words = text.toLowerCase().split(" ");
+      for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        if (britishOnly[word]) {
+          words[i] = britishOnly[word];
+        }
+      }
+      return words.join(" ");
+    } else {
+      throw new Error("Invalid locale");
+    }
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,28 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+
+    if (!text || !locale) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+
+     if (text == "") {
+       return res.json({ error: "No text to translate" });
+     }
+
+    if (Translator.locales.indexOf(locale) === -1) {
+      return res.json({ error: "Invalid value for locale field" });
+    }
+
+    console.log("Text and Locale: ", text, locale);
+    const translation = translator.translate(text, locale);
+    console.log("Translation: ", translation);
+    res.json({ text, translation });
+  });
 };
